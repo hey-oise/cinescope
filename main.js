f87b1ad5c482dbe7cacb600aebe7eb4468a8b555
hey-oise <oise.giwa.okugbe@gmail.com>
2025-02-01 00:52:09 +0100

    init

--- main.js
+++ main.js
@@ -1,11 +1,54 @@
-const APILINK = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=f584fe697e29a069124ef2d7764ea510&pages=1";
+const APILINK =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=f584fe697e29a069124ef2d7764ea510&pages=1";
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?api_key=f584fe697e29a069124ef2d7764ea510&query=";
-
+const SEARCHAPI =
+  "https://api.themoviedb.org/3/search/movie?api_key=f584fe697e29a069124ef2d7764ea510&query=";
 
 const main = document.getElementById("section");
 const form = document.getElementById("form");
-const query = document.getElementById("query");
+const search = document.getElementById("query");
 
+returnMovies(APILINK);
+function returnMovies(url) {
+  fetch(url)
+    .then((res) => console.log(res.json()))
+    .then(function (data) {
+      console.log(data.result);
+      data.result.forEach((element) => {
+        const div_card = document.createElement("div");
+        div_card.setAttribute("class", "card");
+        const div_row = document.createElement("div");
+        div_row.setAttribute("class", "row");
+        const div_column = document.createElement("div");
+        div_column.setAttribute("class", "column");
+        const image = document.createElement("img");
+        image.setAttribute("class", "tumbnail");
+        image.setAttribute("id", "image");
+        const title = document.createElement("div");
+        div_card.setAttribute("class", "card");
+        const center = document.createElement("center");
 
-function returnMovies
+        title.innerHTML = `${element.title}`;
+        image.src = IMG_PATH + element.poster_path;
+
+        center.appendChild(image);
+        div_card.appendChild(center);
+        div_card.appendChild(title);
+        div_column.appendChild(div_card);
+        div_row.appendChild(div_column);
+
+        main.appendChild(div_row);
+      });
+    });
+}
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  main.innerHTML = "";
+
+  const searchItem = search.value;
+
+  if (searchItem) {
+    returnMovies(SEARCHAPI + searchItem);
+    search.value = "";
+  }
+});
